ea57aff01ad3826d8c17b6d37aff18ac1eaf0549
Filip-92 <filip.peszke.programista@gmail.com>
2021-10-14 13:30:07 +0200

    Adding the Like User feature

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -1,17 +1,7 @@
 namespace API.Helpers
 {
-    public class UserParams
+    public class UserParams : PaginationParams
     {
-        private const int MaxPageSize = 50;
-        public int PageNumber { get; set; } = 1;
-        private int _pageSize = 10;
-
-        public int PageSize
-        {
-            get => _pageSize;
-            set => _pageSize = (value > MaxPageSize) ? MaxPageSize : value;
-        }
-
         public string CurrentUsername { get; set; }
 
         public string Gender { get; set; }
@@ -21,7 +11,5 @@ namespace API.Helpers
         public int MaxAge { get; set; } = 150;
 
         public string OrderBy { get; set; } = "lastActive";
-        
-        
     }
 }
